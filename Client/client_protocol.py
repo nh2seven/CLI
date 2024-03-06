e2bf9e31af2d8286afbbc374b0e34b67f384d30f
nh2seven <nikhil.h@outlook.com>
2024-03-06 12:06:30 +0530

    Server and Client boilerplates created

---
+++ Client/client_protocol.py
@@ -0,0 +1,38 @@
+import socket
+import threading
+
+
+class UDP_Client:
+    # Method to initialize the server with a host and port
+    def __init__(self, host, port):
+        self.client_ip = host # The host IP address
+        self.client_port = port # The host port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Using IPv4 and UDP
+
+    # Socket does not need to be bound to the local host and port, as it is not receiving data
+
+    # Method to send messages
+    def send_message(self):
+        while True:
+            message = input("You: ") # Enter a message
+            self.socket.sendto(message.encode(), (self.client_ip, self.client_port))
+
+    # Method to receive messages
+    def receive_message(self):
+        while True:
+            data = self.socket.recvfrom(1024)
+            data = data[0].decode()
+            print(f"\nUnknown: {data}")
+
+    # Method to start the threads
+    def start_threads(self): 
+        receive = threading.Thread(target=self.receive_message)
+        send = threading.Thread(target=self.send_message)
+
+        receive.start()
+        send.start()
+
+    
+if __name__ == "__main__":
+    server = UDP_Client("127.0.0.1", 5000)
+    server.start_threads()
--- Server/server_protocol.py
+++ Server/server_protocol.py
@@ -5,14 +5,15 @@ import threading
 class UDP_Server:
     # Method to initialize the server with a host and port
     def __init__(self, host, port):
-        self.host_ip = host # The host IP address
-        self.host_port = port # The host port
+        self.server_ip = host # The host IP address
+        self.server_port = port # The host port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Using IPv4 and UDP
 
     # Method to start the server
     def create_socket(self):
-        self.socket.bind((self.host_ip, self.host_port)) # Binds the socket to the local host and port, ready to receive data
-        print(f"Server started on {self.host_ip}:{self.host_port}")
+        self.socket.bind((self.server_ip, self.server_port)) # Binds the socket to the local host and port, ready to receive data
+        print(f"Server started on {self.server_ip}")
+        print(f"Server listening on port {self.server_port}")
 
     # Method to receive messages
     def receive_message(self):
@@ -25,15 +26,21 @@ class UDP_Server:
     def send_message(self):
         while True:
             message = input("You: ") # Enter a message
-            self.socket.sendto(message.encode(), (self.host_ip, self.host_port))
+            self.socket.sendto(message.encode(), (self.server_ip, self.server_port))
 
-    receive = threading.Thread(target=receive_message)
-    send = threading.Thread(target=send_message)
+    # Method to start the threads
+    def start_threads(self): 
+        receive = threading.Thread(target=self.receive_message)
+        send = threading.Thread(target=self.send_message)
 
-    receive.start()
-    send.start()
+        receive.start()
+        send.start()
 
-    
+
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
+
 if __name__ == "__main__":
     server = UDP_Server("127.0.0.1", 5000)
     server.create_socket()
+    server.start_threads()
